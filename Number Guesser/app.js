2e77b398cfdff85b3c794627340a3daf4423c513
logicalOak <mozhnyy.dev@gmail.com>
2022-01-18 00:17:01 +0300

    Записал структуру приложения

--- Number Guesser/app.js
+++ Number Guesser/app.js
@@ -1,102 +1,29 @@
 /*
 ЗАДАЧА ИГРЫ:
-- Игрок должен угадать число от мин до макс
+- Игрок должен угадать число от 1 до 10
 - Игрок получает определенное количество догадок
 - Уведомить игрока об оставшихся догадках
 - Уведомить игрока о правильном ответе
 - Позволить игроку выбрать играть снова
 */
 
-// Game values
-let min = 1,
-	max = 10,
-	winningNum = getRandomNum(min, max),
-	guessesLeft = 3;
-console.log(winningNum);
-
-// UI Elements
-const game = document.querySelector("#game"),
-	minNum = document.querySelector(".min-num"),
-	maxNum = document.querySelector(".max-num"),
-	guessBtn = document.querySelector("#guess-btn"),
-	guessInput = document.querySelector("#guess-input"),
-	message = document.querySelector(".message");
-
-// Назначаем min and max
-minNum.textContent = min;
-maxNum.textContent = max;
-
-//Play again event listener
-game.addEventListener("mousedown", function (e) {
-	if (e.target.className === "play-again") {
-		window.location.reload();
-	}
-});
-
-// Listen for guess
-guessBtn.addEventListener("click", function () {
-	let guess = parseInt(guessInput.value);
-
-	// Validate
-	if (isNaN(guess) || guess < min || guess > max) {
-		setMessage(`Please enter a number between ${min} and ${max}`, "red");
+class GuessNumber {
+	constructor() {
+		this.secretNumber = Math.floor(Math.random() * (10 - 0 + 1)) + 0; // Генерирует случайно число
+		this.counter = 3; // Счетчик
+		this.create(); // Функция для создания разметки
+		this.game(); // Функция с логикой приложения
+		console.clear();
+		console.log(`Псс, загаданное число - ${this.secretNumber}`); // Ответ в консоли для тестирования
 	}
 
-	// Check if won
-	if (guess === winningNum) {
-		// Game over - won
-		gameOver(true, `${winningNum} is correct, YOU WIN`);
-	} else {
-		// Wrong number
-		guessesLeft -= 1;
+	// Генерация и показ сообщений
 
-		if (guessesLeft === 0) {
-			// Game over - lost
-			gameOver(
-				false,
-				`Game over, you lost. The correct number was ${winningNum}`
-			);
-		} else {
-			// Game continues - answer wrong
-			// Change border color
-			guessInput.style.borderColor = "red";
+	// Формирование логики
 
-			// Clear input
-			guessInput.value = "";
-			// Tell user its the wrong number
-			setMessage(
-				`${guess} is not correct, ${guessesLeft} guesses left`,
-				"red"
-			);
-		}
-	}
-});
+	// Перезагрузка страницы
 
-// Game over
-function gameOver(won, msg) {
-	let color;
-	won === true ? (color = "green") : (color = "red");
-	// Disable input
-	guessInput.disabled = true;
-	// Change border color
-	guessInput.style.borderColor = color;
-	// Set text borderColor
-	message.style.color = color;
-	// Set message
-	setMessage(msg);
+	// Создание разметки
 
-	//Play agian?
-	guessBtn.value = "Play Again";
-	guessBtn.className += "play-again";
-}
-
-// Get Random number
-function getRandomNum(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// Set message
-function setMessage(msg, color) {
-	message.style.color = color;
-	message.textContent = msg;
+	// Новый экземпляр класса
 }
